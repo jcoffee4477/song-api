0f3350da163ab27e4efb1485dc7ee5643ee32a10
jcoffee4477 <jacobcoffee@jacobs-air.mynetworksettings.com>
2023-08-02 15:09:47 -0400

    add create/show

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,28 @@
 class SongsController < ApplicationController
+
+def index
+  @songs = Song.all
+  render :index
+end
+
+def show
+  @song = Song.find_by(id: params[:id])
+
+  render :show
+end
+
+def create 
+  @song = Song.new(
+    title: params[:title],
+    album: params[:album],
+    artist: params[:artist],
+    year: params[:year]
+  )
+  @song.save
+  render :show
+end
+
+def
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/songs" => "songs#index"
+  get "/songs" => "songs#show"
+  post "/songs" => "songs#create"
+
 end
